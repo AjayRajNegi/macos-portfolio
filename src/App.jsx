36e0ddd4b9287cb64fd1d7b13303e5e43ab2df84
AjayRajNegi <ajayrajnegi111@gmail.com>
2025-12-08 23:00:08 +0530

    Navbar added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import Navbar from "#components/Navbar";
 import React from "react";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold">Welcome</h1>;
+  return (
+    <main>
+      <Navbar />
+    </main>
+  );
 };
 
 export default App;
