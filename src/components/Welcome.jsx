cf1cb640f971caaa13213d70e50394a038f62ac2
AjayRajNegi <ajayrajnegi111@gmail.com>
2025-12-09 23:11:35 +0530

    Welcome component for the main section

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,32 @@
+import React, { useRef } from "react";
+
+const renderText = (text, className, baseWeight = 400) => {
+  return [...text].map((char, i) => (
+    <span
+      key={i}
+      className={className}
+      style={{ fontVariantionSettings: `'whgt ${baseWeight}` }}
+    >
+      {char === " " ? "\u00A0" : char}
+    </span>
+  ));
+};
+
+const Welcome = () => {
+  const titleRef = useRef(null);
+  const subtitleRef = useRef(null);
+
+  return (
+    <section id="welcome">
+      <p ref={subtitleRef}>Hey, I'm Ajay! Welcome to my</p>
+      <h1 ref={titleRef} className="mt-7">
+        portfolio
+      </h1>
+      <div className="small-screen">
+        <p>This portfolio is designed for desktop/tablet screens only.</p>
+      </div>
+    </section>
+  );
+};
+
+export default Welcome;
