46a876672ba530dceaf179fa2fdf5aa6c58b3d42
AjayRajNegi <ajayrajnegi111@gmail.com>
2025-12-11 12:08:39 +0530

    Photos window added

--- src/windows/index.js
+++ src/windows/index.js
@@ -4,5 +4,6 @@ import Terminal from "./Terminal";
 import Finder from "./Finder";
 import Text from "./Text";
 import Contact from "./Contact";
+import Photos from "./Photos";
 
-export { Terminal, Safari, Resume, Finder, Text, Contact };
+export { Terminal, Safari, Resume, Finder, Text, Contact, Photos };
